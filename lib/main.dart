2a9a43d2ba2ca8744d3a0af4e2323db08730a64d
Byeong Gui Jeong <jsmile@naver.com>
2023-08-01 19:33:06 +0900

    MultiBlocProvider()

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,8 @@ import 'package:cubit2cubit_listener/cubits/color/color_cubit.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
+import 'cubits/counter/counter_cubit.dart';
+
 void main() {
   runApp(const MyApp());
 }
@@ -11,14 +13,19 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter BlocListener for Cubit to Cubit',
-      theme: ThemeData(
-        useMaterial3: true,
-      ),
-      home:
-          const MyHomePage(title: 'BlocListener for Cubit to Cubit Home Page'),
-    );
+    return MultiBlocProvider(
+        providers: [
+          BlocProvider(create: (context) => ColorCubit()),
+          BlocProvider(create: (context) => CounterCubit()),
+        ],
+        child: MaterialApp(
+          title: 'Flutter BlocListener for Cubit to Cubit',
+          theme: ThemeData(
+            primarySwatch: Colors.blue,
+          ),
+          home: const MyHomePage(
+              title: 'BlocListener for Cubit to Cubit Home Page'),
+        ));
   }
 }
 
@@ -29,31 +36,46 @@ class MyHomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return BlocListener<ColorCubit, ColorState>(
-      listener: (context, state) {},
-      child: Scaffold(
-        appBar: AppBar(
-          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-          title: Text(title),
-        ),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              const Text(
-                'You have pushed the button this many times:',
+    return Scaffold(
+      backgroundColor: Colors.red,
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            ElevatedButton(
+              child: const Text(
+                'Change Color',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 24.0,
+                ),
               ),
-              Text(
-                '0',
-                style: Theme.of(context).textTheme.headlineMedium,
+              onPressed: () {},
+            ),
+            const SizedBox(height: 20.0),
+            const Text(
+              '0',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 24.0,
               ),
-            ],
-          ),
-        ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: () {},
-          tooltip: 'Increment',
-          child: const Icon(Icons.add),
+            ),
+            const SizedBox(height: 20.0),
+            ElevatedButton(
+              child: const Text(
+                'Increse Count',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 24.0,
+                ),
+              ),
+              onPressed: () {},
+            ),
+          ],
         ),
       ),
     );
